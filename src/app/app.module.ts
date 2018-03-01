93774830573a3a1ce73b157a1a9170947b9da9d0
anilad <aniladdao@gmail.com>
2018-03-01 14:21:22 -0800

    created components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FaqComponent } from './faq/faq.component';
+import { MapComponent } from './map/map.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { FoodComponent } from './food/food.component';
+import { VendorsComponent } from './vendors/vendors.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FaqComponent,
+    MapComponent,
+    ScheduleComponent,
+    FoodComponent,
+    VendorsComponent
   ],
   imports: [
     BrowserModule,
