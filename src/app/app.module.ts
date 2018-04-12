0161606635aecf58d8e31d2ee6a039c434ecc294
dbellonzi <32828035+dbellonzi@users.noreply.github.com>
2018-04-12 16:23:54 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
 import { FaqComponent } from './faq/faq.component';
 import { MapComponent } from './map/map.component';
@@ -28,7 +27,6 @@ import { ArtistsComponent } from './artists/artists.component';
   declarations: [
     AppComponent,
     FooterComponent,
-    AboutComponent,
     HeaderComponent,
     FaqComponent,
     MapComponent,
